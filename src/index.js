fbf8c87dfa3a3c0c42f3e0e89c6ec9857cc2fcd6
Selim Fazylov <selim.fazylov1@gmail.com>
2022-02-20 04:59:29 +0200

    task 10 step 1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,67 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries.js';
+var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('#search-box');
+const list = document.querySelector('.country-list');
+const info = document.querySelector('.country-info');
+
+const languages = {
+    eng: "English",
+    smo: "Samoan",
+};
+    
+console.log();
+
+input.addEventListener("input", debounce(searchFunction, DEBOUNCE_DELAY));
+
+function searchFunction() {
+    const value = input.value.trim();
+    list.innerHTML = '';
+    info.innerHTML = '';
+    if (!value) {
+        return;
+    }
+    fetchCountries(value).then((data) => {
+        console.log(data);
+        if (data.length > 10) {
+            return console.log("Too many matches found. Please enter a more specific name.");
+        }
+        if (data.length > 1 && data.length <= 10) {
+            renderList(data);
+            return;
+        }
+        if (data.length === 1) {
+            const markup = countryCard(data);
+            info.innerHTML = markup;
+            return;
+        }
+    });
+}
+
+function renderList(data) {
+    const markup = data.map(({ name, flags }) => {
+        return `<li>
+        <img src="${flags.svg}" alt="${name.common}">
+        <p>${name.official}</p>
+      </li>`
+    }).join("");
+    list.insertAdjacentHTML("beforeend", markup);
+}
+
+function countryCard({ name, flags, capital, languages, population }) {
+    console.log(flags);
+    const language = Object.values({...languages});
+    const langString = language.join(", ");
+    return infocard = `<div class="country"
+        ><img src="${flags.svg}" alt="${name.common}" />
+        <p class="country-name">${name.official}</p>
+      </div>
+      <ul class="info-list">
+        <li class="info-list__item"><span class="info-list__span">Capital : </span>${capital}</li>
+        <li class="info-list__item"><span class="info-list__span">Population : </span>${population}</li>
+        <li class="info-list__item"><span class="info-list__span">Languages : </span>${langString}</li>
+      </ul>`;
+}
